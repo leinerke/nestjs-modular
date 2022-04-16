8e8a14aa78c3012d8feb37463ccd8a9058db7906
Leinerke Suarez <leinerke2000@gmail.com>
2022-04-16 08:12:53 -0500

    Tipado en config.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import config from './config';
+import { ConfigType } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+  ) {}
+
   getHello(): string {
-    return 'Hello World!';
+    return `Hello World! ${this.configService.apiKey} ${this.configService.database.name}`;
   }
 }
